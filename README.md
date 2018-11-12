3cdf2f5cf65af049566b339fbacf4cbc108bf63a
Fabian Schliski <me@fabianschliski.de>
2018-11-12 18:28:27 +0100

    Rendering component with default settings

---
+++ README.md
@@ -0,0 +1,27 @@
+// TODO status icons
+
+## Features
+
+// TODO GIF
+- // TODO features
+
+## Install
+
+```bash
+$ npm install --save antd-password-input-strength
+```
+or
+```bash
+$ yarn add --save antd-password-input-strength
+```
+
+## Usage
+
+// TODO usage + props table
+// + example with antd form
+
+// TODO publish on awesome-antd & reddit
+
+# License
+
+MIT License
--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react'
 import * as renderer from 'react-test-renderer'
-import {PasswordInput} from '../src'
+import { PasswordInput, PasswordInputProps, PasswordInputSettings } from '../src'
 
 describe('PasswordInput', () => {
 
   /** Snapshot testing */
   it('should render correctly', () => {
+    const settings: PasswordInputSettings = {
+      colorScheme: {
+        levels: ["#ff4033", "#fe940d", "#ffd908", "#cbe11d", "#6ecc3a"],
+        noLevel: "lightgrey"
+      },
+      height: 3,
+      alwaysVisible: false
+    };
     const component = renderer.create(
-      <PasswordInput />
+      <PasswordInput
+        settings={settings}
+      />
     );
     let tree = component.toJSON();
 
